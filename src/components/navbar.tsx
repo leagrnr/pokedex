c1d1661abb0d397d2902a2f2fd1dd7d84b967cc8
ElRacoon <lea.granier@outlook.fr>
2025-02-01 20:33:20 +0100

    add navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+export function Navbar() {
+    return (
+        <nav className="navbar">
+           <ul>
+                <li><a href="/pokemon">Pokémon</a></li>
+                <li><a href="/item">Item</a></li>
+           </ul>
+        </nav>
+    );
+}
